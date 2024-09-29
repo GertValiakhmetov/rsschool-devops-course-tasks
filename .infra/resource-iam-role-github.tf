aea0a32602404a7da9aee6bd5f48b909161b1d73
GertValiakhmetov <valigertt@gmail.com>
2024-09-29 22:13:52 +0300

    chore: test

--- .infra/resource-iam-role-github.tf
+++ .infra/resource-iam-role-github.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "github_oidc_role" {
       {
         "Effect" : "Allow",
         "Principal" : {
-          "Federated" : "arn:aws:iam::372376449465:oidc-provider/token.actions.githubusercontent.com"
+          "Federated" : aws_iam_openid_connect_provider.github.arn
         },
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
