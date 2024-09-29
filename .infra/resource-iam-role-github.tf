4615ddacd0b3f65aeb295ca9220daea16e9d3008
GertValiakhmetov <valigertt@gmail.com>
2024-09-29 20:04:04 +0300

    fix: change string comparing method

--- .infra/resource-iam-role-github.tf
+++ .infra/resource-iam-role-github.tf
@@ -11,7 +11,7 @@ resource "aws_iam_role" "github_oidc_role" {
         },
         "Action" : "sts:AssumeRoleWithWebIdentity",
         "Condition" : {
-          "StringEquals" : {
+          "StringLike" : {
             "token.actions.githubusercontent.com:sub" : "repo:GertValiakhmetov/rsschool-devops-course-tasks:ref:refs/heads/*"
           }
         }
